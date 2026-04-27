9b4d3a5a2f0ec3a257ab06a8e8356c4056919fe2
ziadAlattar <ziadalattar22@gmail.com>
2026-04-27 05:16:56 +0300

    V2

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 mongoose.set('strictQuery', true);
 const connection = async ()=>{
-    return await mongoose.connect("mongodb+srv://ziad:00241300@cluster0.bxnbg.mongodb.net/gallery")
+    return await mongoose.connect("mongodb+srv://ZiadAlmorsy:00241300@cluster0.mjwgrkh.mongodb.net/gallery")
     .then(()=> console.log(`connected on ...... `))
     .catch(err=>console.log(`fail to connect `))
 }
--- README.md
+++
@@ -1,1 +0,0 @@
-# galleryApis
